cffb266411f0097a2e3743ad890f04c959f4b82f
Denakh <denakhokh@gmail.com>
2019-03-28 00:44:05 +0200

    First demo

--- test_body_with_request.py
+++ test_body_with_request.py
@@ -22,10 +22,10 @@ def main_test_function(test_data):
         "actual status code doesn't match expected"
     if test_data["expected_error_message"] == "":
         if test_data["validation_type"] == "value":
-            assert check_subdictionary_using_param_values(response_body, test_data["expected_json_part"]), \
+            assert check_dictionary_using_param_values(response_body, test_data["expected_json_part"]), \
                 "actual JSON param value(s) doesn't match expected"
         else:
-            assert check_subdictionary_using_param_types(response_body, test_data["expected_json_part"]), \
+            assert check_dictionary_using_param_types(response_body, test_data["expected_json_part"]), \
                 "actual JSON param type(s) doesn't match expected"
     else:
         assert test_data["actual_error_message"] == test_data["expected_error_message"], \
@@ -33,36 +33,69 @@ def main_test_function(test_data):
     print("PASSED ;")
 
 
-def check_subdictionary_using_param_values(int_dict_whole, int_dict_part):
-    int_dpk_list = int_dict_part.keys()
+def check_dictionary_using_param_values(dict_whole, dict_part):
+    int_dpk_list = dict_part.keys()
     for key in int_dpk_list:
-        if key not in int_dict_whole:
+        if key not in dict_whole:
             return False
         else:
-            if type(int_dict_part[key]) == dict and type(int_dict_whole[key]) == dict:
-                subdict_p = int_dict_part[key]
-                subdict_w = int_dict_whole[key]
-                if check_subdictionary_using_param_values(subdict_w, subdict_p) is False:
+            if type(dict_part[key]) == dict and type(dict_whole[key]) == dict:
+                subdict_p = dict_part[key]
+                subdict_w = dict_whole[key]
+                if check_dictionary_using_param_values(subdict_w, subdict_p) is False:
+                    return False
+            elif type(dict_part[key]) == list and type(dict_whole[key]) == list:
+                int_list_part = dict_part[key]
+                int_list_whole = dict_whole[key]
+                if check_list_using_param_values(int_list_whole, int_list_part) is False:
                     return False
             else:
-                if int_dict_part[key] != int_dict_whole[key]:
+                if dict_part[key] != dict_whole[key]:
                     return False
     return True
 
 
-def check_subdictionary_using_param_types(int_dict_whole, int_dict_part):
-    int_dpk_list = int_dict_part.keys()
+def check_dictionary_using_param_types(dict_whole, dict_part):
+    int_dpk_list = dict_part.keys()
     for key in int_dpk_list:
-        if key not in int_dict_whole:
+        if key not in dict_whole:
             return False
         else:
-            if type(int_dict_part[key]) == dict and type(int_dict_whole[key]) == dict:
-                subdict_p = int_dict_part[key]
-                subdict_w = int_dict_whole[key]
-                if check_subdictionary_using_param_types(subdict_w, subdict_p) is False:
+            if type(dict_part[key]) == dict and type(dict_whole[key]) == dict:
+                subdict_p = dict_part[key]
+                subdict_w = dict_whole[key]
+                if check_dictionary_using_param_types(subdict_w, subdict_p) is False:
                     return False
             else:
-                print()
-                if not isinstance(int_dict_part[key], type(int_dict_whole[key])):
+                if not isinstance(dict_part[key], type(dict_whole[key])):
                     return False
     return True
+
+
+def check_list_using_param_values(list_whole, list_part):
+    sub_list_w = []
+    sub_list_p = []
+    for param in list_part:
+        if type(param) == dict:
+            sub_list_p.append(param)
+    for param in list_whole:
+        if type(param) == dict:
+            sub_list_w.append(param)
+    if len(sub_list_p) > len(sub_list_w):
+        return False
+    i = 0
+    l = 0
+    while i < len(sub_list_p):
+        mark = False
+        j = l+1
+        while j < len(sub_list_w):
+            if check_dictionary_using_param_values(sub_list_w[j], sub_list_p[i]):
+                mark = True
+                l = j
+                break
+            j += 1
+        if mark is False:
+            return False
+
+
+
